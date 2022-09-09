3581b11d53b9a2f5c9e4a392676723f7e434eb04
DiegoVillacis <95387849+DiegoVillacis@users.noreply.github.com>
2022-09-09 12:00:17 -0500

    COM

--- lib/routes/routes_generation.dart
+++ lib/routes/routes_generation.dart
@@ -8,33 +8,4 @@ import 'package:pcex_sistema/ui/pays/pays_page.dart';
 import 'package:pcex_sistema/ui/reports/reports_page.dart';
 import 'package:pcex_sistema/ui/task/task_page.dart';
 
-class RouteGenerator {
-  static Route<dynamic> generatorRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case routeDashboard:
-        return MaterialPageRoute(builder: (_) => DashboardPage());
-        break;
-      case routeClient:
-        return MaterialPageRoute(builder: (_) => ClientsPage());
-        break;
-      case routeInvoice:
-        return MaterialPageRoute(builder: (_) => InvoicesPage());
-        break;
-      case routePays:
-        return MaterialPageRoute(builder: (_) => PaysPage());
-        break;
-      case routeReports:
-        return MaterialPageRoute(builder: (_) => ReportsPage());
-        break;
-      case routeTask:
-        return MaterialPageRoute(builder: (_) => TaskPage());
-        break;
-    }
-  }
-}
-
-<<<<<<< HEAD
-//xd
-=======
->>>>>>> af74702ba7311aced78b03c03d2253331e90bb73
-//
+class RouteGenerator {}
